d0d916cf8bd47c6535a008eb2249c201e8f9efee
饶大奇 <598571284@qq.com>
2017-12-03 13:35:32 +0800

    更改界面

--- app.js
+++ app.js
@@ -12,6 +12,18 @@ require('./cloud');
 
 var app = express();
 
+var requireAuthentication = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+}
+
+//设置跨域访问
+app.all('/v1/*', requireAuthentication);
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
